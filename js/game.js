d2182f52f0ed109bb9834053ee7c52fdfc6a5f79
Manon Lemasson <manon.lemasson@hotmail.fr>
2021-03-02 11:18:48 +0100

    checkGameOver ok mais a optimiser

--- js/game.js
+++ js/game.js
@@ -29,7 +29,7 @@ const game = {
             alert('Déjà pris !!');
             }
             else {grid.cells[rowIndex][columnIndex] = 'cr';
-        game.countCr++;}
+                game.countCr++;}
         }
 
         else {
@@ -51,7 +51,7 @@ const game = {
     
 
         // On appelle la méthode checkGameOver qui vérifie si le jeu est terminé ou non
-        //game.checkGameOver();
+        game.checkGameOver();
     
         
     },
@@ -59,8 +59,96 @@ const game = {
      isEven : function(number) {
         // le reste de sa division par 2 est 0
         return ((number % 2) === 0);
+    },
+
+    checkGameOver : function () {
+        
+        if (grid.cells[0][0] === 'cr' && grid.cells[0][1] === 'cr' && grid.cells[0][2] === 'cr') {
+            alert('Bravo! Joueur 1 gagne');
+            document.location.reload();
+        }
+        else if (grid.cells[0][0] === 'ci' && grid.cells[0][1] === 'ci' && grid.cells[0][2] === 'ci') {
+            alert('Bravo! Joueur 2 gagne');
+            document.location.reload();
+        }
+        else if (grid.cells[1][0] === 'cr' && grid.cells[1][1] === 'cr' && grid.cells[1][2] === 'cr') {
+            alert('Bravo! Joueur 1 gagne');
+            document.location.reload();
+        }
+        else if (grid.cells[1][0] === 'ci' && grid.cells[1][1] === 'ci' && grid.cells[1][2] === 'ci') {
+            alert('Bravo! Joueur 2 gagne');
+            document.location.reload();
+        }
+        else if (grid.cells[2][0] === 'cr' && grid.cells[2][1] === 'cr' && grid.cells[2][2] === 'cr') {
+            alert('Bravo! Joueur 1 gagne');
+            document.location.reload();
+        }
+        else if (grid.cells[2][0] === 'ci' && grid.cells[2][1] === 'ci' && grid.cells[2][2] === 'ci') {
+            alert('Bravo! Joueur 2 gagne');
+            document.location.reload();
+        }
+        else if (grid.cells[0][0] === 'cr' && grid.cells[1][0] === 'cr' && grid.cells[2][0] === 'cr') {
+            alert('Bravo! Joueur 1 gagne');
+            document.location.reload();
+        }
+        else if (grid.cells[0][0] === 'ci' && grid.cells[1][0] === 'ci' && grid.cells[2][0] === 'ci') {
+            alert('Bravo! Joueur 2 gagne');
+            document.location.reload();
+        }
+        else if (grid.cells[0][1] === 'cr' && grid.cells[1][1] === 'cr' && grid.cells[2][1] === 'cr') {
+            alert('Bravo! Joueur 1 gagne');
+            document.location.reload();
+        }
+        else if (grid.cells[0][1] === 'ci' && grid.cells[1][1] === 'ci' && grid.cells[2][1] === 'ci') {
+            alert('Bravo! Joueur 2 gagne');
+            document.location.reload();
+        }
+        else if (grid.cells[0][2] === 'cr' && grid.cells[1][2] === 'cr' && grid.cells[2][2] === 'cr') {
+            alert('Bravo! Joueur 1 gagne');
+            document.location.reload();
+        }
+        else if (grid.cells[0][2] === 'ci' && grid.cells[1][2] === 'ci' && grid.cells[2][2] === 'ci') {
+            alert('Bravo! Joueur 2 gagne');
+            document.location.reload();
+        }
+        else if (grid.cells[0][0] === 'cr' && grid.cells[1][1] === 'cr' && grid.cells[2][2] === 'cr') {
+            alert('Bravo! Joueur 1 gagne');
+            document.location.reload();
+        }
+        else if (grid.cells[0][0] === 'ci' && grid.cells[1][1] === 'ci' && grid.cells[2][2] === 'ci') {
+            alert('Bravo! Joueur 2 gagne');
+            document.location.reload();
+        }
+        else if (grid.cells[0][2] === 'cr' && grid.cells[1][1] === 'cr' && grid.cells[2][0] === 'cr') {
+            alert('Bravo! Joueur 1 gagne');
+            document.location.reload();
+        }
+        else if (grid.cells[0][2] === 'ci' && grid.cells[1][1] === 'ci' && grid.cells[2][0] === 'ci') {
+            alert('Bravo! Joueur 2 gagne');
+            document.location.reload();
+        }
+
+        let remainingEmpty = 0;
+        for (let row of grid.cells) {
+            for(let cell of row) {
+                // on comptabilise s'il reste des cases vides dans la grille 
+                if(cell === ''){
+                    remainingEmpty++;
+                }
+            }
+        }
+
+        if (remainingEmpty === 0 ){
+            alert('Match nul');
+            document.location.reload();
+        }
+        
     }
 
 }
 
-
+/* grid.cells = [
+            ['00', '01', '02'],
+            ['10', '11', '12'],
+            ['20', '21', '22']
+        ] */
